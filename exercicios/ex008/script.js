7704b31d7683ab786c28934504fa515eeee7c6c1
Arthur Henrique Pestana Schneider <92184381+arthurpestana@users.noreply.github.com>
2021-11-10 13:48:05 -0300

    Exercícios de JavaScript

---
+++ exercicios/ex008/script.js
@@ -0,0 +1,9 @@
+var velo = 223
+console.log("A velocidade do seu carro é de " + velo + " Km/h");
+if (velo > 60){ // Condição Simples!
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!!");
+}
+if (velo > 120){
+    console.log("Tá querendo morrer também, né?")
+}
+console.log("Dirija sempre usando cinto de segurança!");
---
+++ exercicios/ex009/script.js
@@ -0,0 +1,8 @@
+var country = "Brasil"
+console.log(`Você está vivendo no país: ${country}`)
+if (country == "Brasil"){
+    console.log("Você é Brasileiro!")
+}
+else {
+    console.log("Você é estrangeiro!")
+}
