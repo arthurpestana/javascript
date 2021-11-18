8bc3ee8a9893ce35b37a3055d7bb2265a419ea18
Arthur Henrique Pestana Schneider <92184381+arthurpestana@users.noreply.github.com>
2021-11-18 20:38:29 -0300

    ex 017

---
+++ exercicios/ex017/script.js
@@ -0,0 +1,17 @@
+console.log("Vai começar...")
+for (var cont=1; cont<=6; cont++) {
+    console.log(`Passo ${cont}`)
+}
+console.log("FIM!")
+
+/*var cont = 1
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont<=6)
+
+var cont = 1
+while (cont<=6){
+    console.log(`Passo ${cont}`)
+    cont++
+}*/
