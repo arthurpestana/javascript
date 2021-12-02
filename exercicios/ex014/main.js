e347e39bb3d2ace1938207ee01bc8a27237865fa
Arthur Henrique Pestana Schneider <92184381+arthurpestana@users.noreply.github.com>
2021-12-02 19:38:02 -0300

    ex014

--- exercicios/ex014/main.js
+++ exercicios/ex014/main.js
@@ -1,7 +1,51 @@
-let btn = document.querySelector("input#button")
-btn.addEventListener("click", adicionar)
+let res = document.querySelector("div#res")
+let num = document.querySelector("input#number")
+let list = document.querySelector("#lista")
+let valores = []
 
-function adicionar() {
-    let num = []
-    
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert("Adicione valores antes de finalizar!")
+    }
+    else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma/tot
+        res.innerHTML = ""
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+    }
 }
